aa4514cfbdcb73e4fa54299e3bfeceb85c53cac4
arman0001900 <armananaryan771@gmail.com>
2025-11-17 21:10:15 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 
-TOKEN = "8282818497:AAFLfLaGp8zJ4_UXYXgP0V9488vyamLKExs
+TOKEN = "8282818497:AAFLfLaGp8zJ4_UXYXgP0V9488vyamLKExs"
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
 last_update_id = 0
